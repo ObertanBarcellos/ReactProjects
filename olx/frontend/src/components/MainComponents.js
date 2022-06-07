c206547d12add7e41477b3529c1a60b56d470671
ObertanBarcellos <80553319+ObertanBarcellos@users.noreply.github.com>
2022-06-07 04:28:06 -0300

    OLX Api setting

--- olx/frontend/src/components/MainComponents.js
+++ olx/frontend/src/components/MainComponents.js
@@ -13,3 +13,12 @@ export const PageContainer = styled.div`
 export const PageTitle = styled.h1``;
 
 export const PageBody = styled.div``;
+
+export const ErrorMessage = styled.div`
+    margin: 10px 0;
+    background-color: #FFCACA;
+    color: #000;
+    border: 2px solid #FF0000;
+    padding: 10px;
+    text-align: center;
+`;
--- olx/frontend/src/pages/SignIn/index.js
+++ olx/frontend/src/pages/SignIn/index.js
@@ -3,7 +3,7 @@ import { PageArea } from "./styled";
 import useApi from "../../helpers/OlxAPI";
 import { doLogin } from "../../helpers/AuthHendler";
 
-import { PageContainer, PageTitle } from "../../components/MainComponents";
+import { PageContainer, PageTitle, ErrorMessage } from "../../components/MainComponents";
 
 const Page = () => {
     const api = useApi();
@@ -26,6 +26,7 @@ const Page = () => {
             doLogin(json.token, rememberPassword);
             window.location.href = '/'; 
         }
+        setDisabled(false);
     }
 
     return (
@@ -36,23 +37,42 @@ const Page = () => {
 
             <PageArea>
 
+                { error && <ErrorMessage> { error } </ErrorMessage> }
+
                 <form onSubmit={handleSubmit}>
                     <label className="area">
                         <div className="area--title">Email</div>
                         <div className="area--input">
-                            <input type="email" disabled={disabled} />
+                            <input 
+                                type="email" 
+                                disabled={disabled} 
+                                value={email}
+                                onChange={e=>setEmail(e.target.value)}
+                                required
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Senha</div>
                         <div className="area--input">
-                            <input type="password" disabled={disabled} />
+                            <input 
+                                type="password" 
+                                disabled={disabled} 
+                                value={password}
+                                onChange={e=>setPassword(e.target.value)}
+                                required
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Lembrar senha</div>
                         <div className="area--input">
-                            <input type="checkbox" disabled={disabled} />
+                            <input 
+                                type="checkbox" 
+                                disabled={disabled} 
+                                checked={rememberPassword}
+                                onChange={() => setRememberPassword(!rememberPassword)}
+                            />
                         </div>
                     </label>
                     <label className="area">
